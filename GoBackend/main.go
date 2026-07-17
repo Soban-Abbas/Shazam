47bbf43807bfa5e4f9a4383db809360192348878
Soban Abbas <sobanabbass@gmail.com>
2026-07-17 08:44:54 +0500

    updating folder structure

--- GoBackend/main.go
+++ GoBackend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"shazam-backend/src/routes"
 
 	"github.com/gin-gonic/gin"
@@ -13,7 +14,16 @@ import (
 
 func main() {
 	router := gin.Default()
-router.Use(cors.Default())
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	routes.FileRoutes(router)
 
 	router.Run(":8080")
